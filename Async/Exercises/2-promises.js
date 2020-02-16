06da6f9aae47b79fd0097133e2af670e11a7ce3c
jaime <jaime.molina@mattersupply.co>
2020-02-15 22:35:02 -0500

    exercise promise

--- Async/Exercises/2-promises.js
+++ Async/Exercises/2-promises.js
@@ -1,5 +1,4 @@
 /**
-/**
  * Dado la siguiente cadena de Strings
  * ponerlas en mayusculas y cada uno hacerlo
  * una promesa.
@@ -12,3 +11,15 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 
 const animals = ['sariguella', 'vaca', 'iguana', 'peresoso']
+const promisesAnimals = animals.map(animal => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(animal.toUpperCase())
+    }, 1000)
+  })
+  return promise
+})
+
+Promise.all(promisesAnimals).then(promisedAnimals => {
+  console.log('TCL: promisedAnimals', promisedAnimals)
+})
