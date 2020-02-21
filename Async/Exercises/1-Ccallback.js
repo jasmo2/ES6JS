2baeb134a66ee5b72d1ff162c5b7aa0af8423203
jaime <jaime.molina@mattersupply.co>
2020-02-21 17:57:02 -0500

    C callback function

---
+++ Async/Exercises/1-Ccallback.js
@@ -0,0 +1,14 @@
+/**
+ * Hacer un ciclo Random que vaya entre 1 - 100,
+ * he imprima un string que llega de argumento más el
+ * indice del la iteración actual.
+ *
+ * El truco será hacerlo dentro de un Callback con
+ * Timeout(0)
+ *
+ * ¿Cúal es el que 1º se imprime?
+ * ¿Porqué lo hace?
+ *
+ * Random
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+ */
