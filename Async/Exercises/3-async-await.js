6c3f4adf4cc527e1edb5ee1a1fb57c038f3d86a1
jaime <jaime.molina@mattersupply.co>
2020-02-25 18:39:05 -0500

    promise example

--- Async/Exercises/3-async-await.js
+++ Async/Exercises/3-async-await.js
@@ -8,6 +8,21 @@
 // HINT: usar FETCH de JS para el llamado al API
 // https://dev.to/johnpaulada/synchronous-fetch-with-asyncawait
 
+/**
+ * 
+ * 
+
+ fetch("https://api.github.com/users/jasmo2")
+.then(respose => {
+  //console.log("response :", respose)
+  return respose.json()
+})
+.then(data => {
+ console.log("Github Username",data.login)
+})
+
+ */
+
 const fetch = require('node-fetch')
 const githubUser = ''
 const url = `https://api.github.com/users/${githubUser}`
